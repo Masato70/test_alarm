b73f2c2c4e8874b4b40ef7e6f3df288d36002257
中川真人 <masanew01@gmail.com>
2023-08-26 03:52:43 +0900

    修正

--- lib/main.dart
+++ lib/main.dart
@@ -49,54 +49,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  AudioPlayer audioPlayer = AudioPlayer();
-  String alarmFilePath = "assets/ringtone-126505.mp3";
-  Timer? vibrationTimer;
+  AlarmManager alarmManager = AlarmManager();
 
   @override
   void initState() {
     super.initState();
     AndroidAlarmManager.initialize();
-  }
-
-  void play() {
-    startAlarm();
-    startVib();
-  }
-
-  void stop() {
-    stopAlarm();
-    stopVib();
-  }
-
-  @override
-  void dispose() {
-    audioPlayer.dispose();
-    super.dispose();
-  }
-
-  void startAlarm() async {
-    audioPlayer.play(AssetSource("ringtone-126505.mp3"));
-    audioPlayer.setReleaseMode(ReleaseMode.loop);
-
-    print("アラームを再生中");
-  }
-
-  void stopAlarm() async {
-    await audioPlayer.stop();
-    print("アラームを停止");
-  }
-
-  void startVib() async {
-    vibrationTimer = Timer.periodic(Duration(seconds: 1), (timer) {
-      Vibration.vibrate(duration: 500);
-      print("バイブレーションスタート");
-    });
-  }
-
-  void stopVib() async {
-    vibrationTimer?.cancel();
-    await Vibration.cancel();
   }
 
   @override
@@ -110,12 +68,12 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ElevatedButton(
-              onPressed: play,
+              onPressed:alarmManager.play,
               child: Text("Aボタン - アラーム再生"),
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: stop,
+              onPressed: alarmManager.stop,
               child: Text("Bボタン - アラーム停止"),
             ),
           ],
